2e16a5cf9c57d4ba1407d02403e3600d9a361a67
jblack39 <53546995+jblack39@users.noreply.github.com>
2020-07-31 22:20:15 -0700

    Added cake piece animations to player_token class

--- Trivial_Purfuit/src/player_token/player_token.py
+++ Trivial_Purfuit/src/player_token/player_token.py
@@ -19,6 +19,9 @@ class PlayerToken(QWidget):
         self.x = 0
         self.y = 0
 
+        self.cake_width = 13
+        self.cake_height = self.cake_width
+
         self.board_tile_width  = 0
         self.board_tile_height = self.board_tile_width
 
@@ -45,7 +48,6 @@ class PlayerToken(QWidget):
         """
         return self.cake_list[cake_category]
 
-
     def award_cake_piece(self, cake_category):
         """
         Get a random question of the provided type
@@ -58,6 +60,7 @@ class PlayerToken(QWidget):
 
     def paintEvent(self, event):
         self.draw_player()
+        self.draw_cakes()
     # end paintEvent()
 
 
@@ -67,8 +70,8 @@ class PlayerToken(QWidget):
 
         painter = QPainter(self)
         #painter.setClipRect(QRect(0, 0, 1900, 1900))
-        painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
-        painter.setBrush(QBrush(Qt.darkMagenta, Qt.SolidPattern))
+        painter.setPen(QPen(Qt.lightGray, 5, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.lightGray, Qt.SolidPattern))
 
         if not self.player_initialized:
             self.x = self.board_tile_width * 5  - self.width
@@ -92,7 +95,51 @@ class PlayerToken(QWidget):
             elif direction == "RIGHT":
                 self.x = self.x + (self.board_tile_width * spaces)
 
-
         painter.drawRect(self.x, self.y, self.width, self.height)
         self.draw_token = False
     # end draw_player()
+
+    def draw_cakes(self):
+        painter = QPainter(self)
+        #painter.setClipRect(QRect(0, 0, 1900, 1900))
+
+        #set parameters for center of cake pieces
+        self.people_x_center = self.x + 3
+        self.people_y_center = self.y + 2
+
+        #set parameters for center of cake pieces
+        self.event_x_center = self.x + 19
+        self.event_y_center = self.y + 2
+
+        #set parameters for center of cake pieces
+        self.place_x_center = self.x + 3
+        self.place_y_center = self.y + 21
+
+        #set parameters for center of cake pieces
+        self.holiday_x_center = self.x + 19
+        self.holiday_y_center = self.y + 21
+
+        #draw people cake (default people cake piece color = blue)
+        if self.cake_list["people"]:
+            painter.setPen(QPen(Qt.transparent, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+            painter.drawRect(self.people_x_center,self.people_y_center,self.cake_width,self.cake_height)
+
+        # draw event cake (default people cake piece color = blue)
+        if self.cake_list["event"]:
+            painter.setPen(QPen(Qt.transparent, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
+            painter.drawRect(self.event_x_center, self.event_y_center, self.cake_width, self.cake_height)
+
+        # draw place cake (default people cake piece color = blue)
+        if self.cake_list["place"]:
+            painter.setPen(QPen(Qt.transparent, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.blue, Qt.SolidPattern))
+            painter.drawRect(self.place_x_center, self.place_y_center, self.cake_width, self.cake_height)
+
+        # draw holiday cake (default people cake piece color = blue)
+        if self.cake_list["holiday"]:
+            painter.setPen(QPen(Qt.transparent, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
+            painter.drawRect(self.holiday_x_center, self.holiday_y_center, self.cake_width, self.cake_height)
+    # end draw_cakes()
