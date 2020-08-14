7e6108f830efc34cf5115fa98a32f186830477f9
mattladany <matt@va-a.com>
2020-08-13 22:31:03 -0400

    Added comments to the CircularQueue class

--- Trivial_Purfuit/src/utils/circular_queue.py
+++ Trivial_Purfuit/src/utils/circular_queue.py
@@ -1,8 +1,17 @@
 
 
 class CircularQueue:
+    """
+    A queue in which dequeue() does not remove the item from the queue and
+    once dequeue() gets to the end, it loops back to the front to return the
+    first element in the queue.
+    """
 
     def __init__(self, items=None):
+        """
+        Constructor
+        :param items: The list of items to put in the queue if provided.
+        """
         if items is None:
             items = list()
         self.queue = items
@@ -14,11 +23,20 @@ class CircularQueue:
         self.next_index = 0
 
     def enqueue(self, item):
+        """
+        Enter an item into he queue.
+        :param item: The item to enter.
+        :return: True if successful.
+        """
         self.queue.append(item)
         self.tail = (self.tail + 1)
         return True
 
     def dequeue(self):
+        """
+        Get the next element from the queue, circling back to the front if at the end.
+        :return: The next element from the queue.
+        """
         if len(self.queue) == 0:
             return None
 
@@ -27,4 +45,8 @@ class CircularQueue:
         return item
 
     def size(self):
+        """
+        Get the size of the queue.
+        :return: The queue's size.
+        """
         return len(self.queue)
